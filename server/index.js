06002d640c397b160582f0647794b684d0c25f0d
codycool <cody790410@hotmail.com>
2017-07-04 00:25:07 +0800

    no message

--- server/index.js
+++ server/index.js
@@ -1,23 +1,93 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-const express = require('express');
-const config = require('./config');
-const router = require('./router');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
+var app = require('./app');
+var debug = require('debug')('node-irich:server');
+var http = require('http');
+var models = require('./config/database');
 
+/**
+ * Get port from environment and store in Express.
+ */
 
-let app = express();
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+/**
+ * Create HTTP server.
+ */
 
-router(app);
+var server = http.createServer(app);
 
-app.listen(config.port,()=>console.log("connect"));
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+models.sequelize.sync().then(function() {
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
+  server.listen(port, function() {
+    debug('Express server listening on port ' + server.address().port);
+  });
+  server.on('error', onError);
+  server.on('listening', onListening);
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
